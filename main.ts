e19815c64c2f5b15a968284979c9614dd5a2a88d
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-01 13:16:54 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -130,6 +130,7 @@ namespace eureka_blocks {
   }
 
   //% color="#1E90FF" weight=51 block="待ち時間（秒）|%second|" group="1_信号機ユニット"
+  //% second.min=0 mode.max=60
   export function driveForwards(second: number): void {
     basic.pause(second * 1000);
   }
@@ -253,7 +254,7 @@ namespace eureka_blocks {
     }
   }
 
-  //% color="#525252" weight=18 blockId=eureka_relay block="単体_ﾘﾚｰ |%mode| |%pin|" group="3_ユーレカ装置"
+  //% color="#525252" weight=18 blockId=eureka_relay block="単体_ﾘﾚｰ(ﾃﾞｼﾞﾀﾙ) |%mode| |%pin|" group="3_ユーレカ装置"
   //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=10
   export function eureka_relay(mode: onoff, pin: eureka_IO) {
     switch (pin) {
@@ -277,6 +278,25 @@ namespace eureka_blocks {
         }
     }
   }
+  //% color="#525252" weight=18 blockId=eureka_relay block="FETﾘﾚｰ(ｱﾅﾛｸﾞ出力) |%second| |%pin|" group="3_ユーレカ装置"
+  //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=10
+  //% second.min=-100 mode.max=100
+  export function eureka_relay(mode: number, pin: eureka_IO) {
+    switch (pin) {
+      case eureka_IO.Aﾎﾟｰﾄ:{
+        pins.analogWritePin(AnaloglPin.P0, number);
+      }
+      case eureka_IO.Bﾎﾟｰﾄ:{
+        pins.analogWritePin(AnaloglPin.P1, number);
+      }
+      case eureka_IO.Cﾎﾟｰﾄ:{
+        pins.analogWritePin(AnaloglPin.P2, number);
+      }
+
+    }
+  }
+
+
   //% color="#40a6ff" weight=17 blockId=eureka_white block="単体_LED |%mode| |%pin|" group="3_ユーレカ装置"
   export function eureka_white(port: eureka_IO, mode: onoff) {
     switch (port) {
